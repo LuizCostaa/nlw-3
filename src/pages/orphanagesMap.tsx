781664cf6889877a0347dd08906e10dde9c70943
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-10-13 16:51:50 -0300

    Finalizado primeiro workshop

--- src/pages/orphanagesMap.tsx
+++ src/pages/orphanagesMap.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
 
+import 'leaflet/dist/leaflet.css';
+
 function OrphanagesMap() {
     return (
-        <div className="page-map">
+        <div id="page-map">
             <aside>
                 <header>
                     <img src={mapMarkerImg} alt="Happy" />
@@ -22,7 +25,13 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center={[-26.2515737, -48.7894605]}
+                zoom={14}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
 
             <Link to='' className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
