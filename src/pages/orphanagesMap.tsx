17926b4a02579f0e7f6eb9d6378bf967829d2517
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-10-13 14:30:45 -0300

    Criado os primeiros componentes, rotas e estilos

---
+++ src/pages/orphanagesMap.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+    return (
+        <div className="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas criaças estão esperando sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Joinville</strong>
+                    <span>Santa Catarina</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to='' className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap;
